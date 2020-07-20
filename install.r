885979022a19961b9fd9f8383f278f74fde18fc7
Lincoln Colling <lincoln@colling.net.nz>
2020-07-20 20:38:50 +0100

    Create install.r

---
+++ install.r
@@ -0,0 +1,12 @@
+install.packages("remotes")
+xfun::pkg_attach2("tidyverse")
+xfun::pkg_attach2("emmeans")
+xfun::pkg_attach2("afex")
+xfun::pkg_attach2("psych")
+xfun::pkg_attach2("apa")
+xfun::pkg_attach2("here")'
+xfun::pkg_attach2("ggbeeswarm")
+xfun::install_github("ljcolling/teachingtools")
+install.packages("remotes")
+install.packages("Hmisc")
+install.packages("lm.beta")
